1f1fb00f603ceb9a6173e4d005a808d5ec310470
AlekseyKibyakov <lexa111lexa6@mail.ru>
2023-04-15 12:56:38 +0300

    t

--- parsing_excel.py
+++ parsing_excel.py
@@ -1,15 +1,6 @@
-import json
-from pprint import pprint
-# from openpyxl import load_workbook
 import pandas as pd
-import csv
 
 file = 'MMH_RESPI_s_05.12.2022_13.04.2023-14.04.2023_643952c3ed42dd6e9d2b9572.xlsx'
-
-# with open(file) as f:
-#     reader = csv.DictReader(f)
-#     for row in reader:
-#         pprint(row)
 
 df_mentions = pd.read_excel(file, sheet_name='Упоминания', engine='openpyxl')
 
@@ -23,17 +14,19 @@ df_mentions.columns = titles_list
 
 df_mentions.reset_index(drop=True, inplace=True)
 
-df_mentions.to_csv('test.csv', encoding='utf8')
-	
-# Сброс ограничений на количество выводимых рядов
-pd.set_option('display.max_rows', 50)
+df_mentions.to_excel('test.xlsx', engine='openpyxl')
+
+dates = dict(df_mentions.iloc[:, 0])
+
+
+
+# # Сброс ограничений на количество выводимых рядов
+# pd.set_option('display.max_rows', 50)
  
 # # Сброс ограничений на число столбцов
 # pd.set_option('display.max_columns', None)
  
 # # Сброс ограничений на количество символов в записи
 # pd.set_option('display.max_colwidth', None)
-
-print(df_mentions)
 
 # print(wb.get_sheet_names())
